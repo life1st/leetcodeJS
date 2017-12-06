8b09b56e14b5522cae3f61a54fe3071bce4416f3
Yang <jiaoyangwfzc@gmail.com>
2017-12-06 09:27:41 +0800

    add readme.md

---
+++ readme.md
@@ -0,0 +1,7 @@
+# leetcode-js
+
+最近在leetcode上刷题，备份一下。
+
+使用的语言是javascript
+
+顺带
--- tstest.js
+++ tstest.js
@@ -5,5 +5,6 @@ var arr = [2,4,7,9],
 
 var res = ts(arr, target)
 console.log(`
+arr是：${arr}
 target是：${target}
-被选中的arr位置是：${res}`)
+被选中的arr位置是：${res} (从0开始)`)
