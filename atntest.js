1bca0dfaafda8fd7f94c935d0769a89b0be7db67
Yang <jiaoyangwfzc@gmail.com>
2017-12-09 21:14:20 +0800

    2. Add Two Numbers

---
+++ atntest.js
@@ -0,0 +1,18 @@
+var addTwoNumbers = require('./lib/addTwoNumbers')
+
+var ListNode = require('./lib/listNode')
+/* function ListNode(val){
+    this.val = val
+    this.next = null
+} */
+
+// var l1 = new ListNode(2)
+// l1.append(4).append(3).append(5).append(2).append(0)
+var l1 = new ListNode()
+l1.setVal([1])
+var l2 = new ListNode()
+l2.setVal([9, 9])
+
+var result = addTwoNumbers(l1,l2)
+console.log(result)
+console.log(result.trans2Arr())
