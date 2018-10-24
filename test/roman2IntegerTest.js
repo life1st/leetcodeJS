f8d3e71aab9afd7687fcc5b98a85815f2bd08eaa
life1st <jiaoyangdev@gmail.com>
2018-10-24 12:37:01 +0800

    add: 13.roman2Integer.

---
+++ test/roman2IntegerTest.js
@@ -0,0 +1,6 @@
+const romanToInt = require('../lib/roman2Integer')
+
+console.log(romanToInt("III"))
+console.log(romanToInt("MCMXCIV"))
+console.log(romanToInt("LVIII"))
+console.log(romanToInt("IX"))
